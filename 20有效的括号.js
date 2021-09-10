2ad3ceaae3df866f459201c132e8c2d2aa37f8f9
cheyennee <17362995059@163.com>
2021-09-10 09:20:24 +0800

    #2021/09/10

--- 20有效的括号.js
+++ 20有效的括号.js
@@ -4,33 +4,33 @@
  * @param {string} s
  * @return {boolean}
  */
-//  var isValid = function(s) {
-//     var stack = []
-//     for(let i=0;i<s.length;i++){
-//         if (s[i]==='('||s[i]==='['||s[i]==='{')
-//             stack.push(s[i])
-//         if (s[i]===')'){
-//             if(stack[stack.length-1]==='(')
-//                 stack.pop()
-//             else
-//                 return false
-//         } else if (s[i]===']'){
-//             if(stack[stack.length-1]==='[')
-//                 stack.pop()
-//             else
-//                 return false
-//         } else if (s[i]==='}'){
-//             if(stack[stack.length-1]==='{')
-//                 stack.pop()
-//             else
-//                 return false
-//         }
-//     }
-//     if(stack.length>0)
-//         return false 
-//     else
-//         return true
-// };
+ var isValid = function(s) {
+    var stack = []
+    for(let i=0;i<s.length;i++){
+        if (s[i]==='('||s[i]==='['||s[i]==='{')
+            stack.push(s[i])
+        if (s[i]===')'){
+            if(stack[stack.length-1]==='(')
+                stack.pop()
+            else
+                return false
+        } else if (s[i]===']'){
+            if(stack[stack.length-1]==='[')
+                stack.pop()
+            else
+                return false
+        } else if (s[i]==='}'){
+            if(stack[stack.length-1]==='{')
+                stack.pop()
+            else
+                return false
+        }
+    }
+    if(stack.length>0)
+        return false 
+    else
+        return true
+};
 
 // solution 2: time 60.13%, memory 25.63%
 // 同样使用了栈，也使用了map,这个解法就很优雅,但是效率差不多，而且map得设计合理
